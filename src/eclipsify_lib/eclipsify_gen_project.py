4fe1002e7b21ae4374b6f995c1922d54e69551a6
HannesSommer <hannes.sommer@mavt.ethz.ch>
2014-09-09 12:59:26 +0200

    added optional platform argument - mostly for development

--- src/eclipsify_lib/eclipsify_gen_project.py
+++ src/eclipsify_lib/eclipsify_gen_project.py
@@ -39,6 +39,7 @@ def main(argv=None):
     parser.add_argument("out_dir", nargs=1, help="The output directory. Where to put the eclipse project.")
     parser.add_argument("build_dir", nargs=1, help="This project's build directory.")
     #parser.add_argument("bin_dir", nargs=1, help="This project's bin directory.")
+    parser.add_argument("--platform", help="Platform (defaults to sys.platform).", default=sys.platform)
 
     options = parser.parse_args(argv)
 
@@ -46,6 +47,7 @@ def main(argv=None):
     src_dir     = options.src_dir[0]
     eclipse_dir = options.out_dir[0]
     build_dir   = options.build_dir[0]
+    platform   = options.platform
 
     print("eclipsify!")
     print("----------")
@@ -53,11 +55,11 @@ def main(argv=None):
     print("-- {0}".format(eclipse_dir))
     mkdir_p( os.path.join( eclipse_dir, '.settings' ) )
 
-    if sys.platform == 'darwin':
+    if platform == 'darwin':
         import eclipsify_lib.cproject_osx as cproject
         import eclipsify_lib.project_osx as project
         import eclipsify_lib.language_settings_osx as language_settings
-    elif sys.platform == 'linux2':
+    elif platform == 'linux2':
         import eclipsify_lib.cproject_linux as cproject
         import eclipsify_lib.project_linux as project
         import eclipsify_lib.language_settings_linux as language_settings
