26b342b58743b434cfb3d10603b02b1d69f8d791
HannesSommer <hannes.sommer@mavt.ethz.ch>
2015-09-25 10:02:21 +0200

    Used the new cpp feature to cut out links to source from the templates in case of in source mode (introducing the IN_SOURCE cpp macro, which is defined iff in source mode is active)

--- test/acceptanceTests.py
+++ test/acceptanceTests.py
@@ -10,12 +10,13 @@ class EclipsifyAcceptance(unittest.TestCase):
         unittest.TestCase.__init__(self, *args, **kwargs)
         os.environ['ECLIPSIFY_USE_RELATIVE_LOCATIONS_FOR_TESTING'] = "1"
 
-    def _testGeneratorGeneratesSame(self, platform, projects = ['test_project_1'], testWorkspace = 'testWs/devel'):
+    def _testGeneratorGeneratesSame(self, platform, inSource = False, projects = ['test_project_1'], testWorkspace = 'testWs/devel'):
         for project in projects:
-            outDir = 'expected/%s/%s/%s' % (testWorkspace, platform, project)
+            outDir = 'expected/%s/%s/%s' % (testWorkspace if not inSource else os.path.join(os.path.dirname(testWorkspace), "src"), platform, project)
             subprocess.call(['rm', '-rf', outDir])
             subprocess.call([sys.executable, 
                              '../src/eclipsify', '-v', 
+                             '-s' if inSource else '-v',
                              '-W', testWorkspace, 
                              '-O', outDir, 
                              '--platform', platform, 
@@ -31,8 +32,12 @@ class EclipsifyAcceptance(unittest.TestCase):
             self.assertEqual(c, 0, "There should be no difference between HEAD and the expected output! Either fix the code or commit changed expected output.")
     def testLinux(self):
         self._testGeneratorGeneratesSame('linux2')
+    def testLinuxInSource(self):
+        self._testGeneratorGeneratesSame('linux2', True)
     def testDarwin(self):
         self._testGeneratorGeneratesSame('darwin')
+    def testDarwinInSource(self):
+        self._testGeneratorGeneratesSame('darwin', True)
 if __name__ == '__main__':
     import rostest
     rostest.rosrun('eclipsify', 'acceptance', EclipsifyAcceptance)
