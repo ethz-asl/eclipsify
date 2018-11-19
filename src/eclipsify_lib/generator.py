2947aa8aa7e4c37a0e0dbc371fe132f82e28bc3e
HannesSommer <hannes.sommer@mavt.ethz.ch>
2018-11-19 08:04:19 +0100

    Whitespace cleanup

--- src/eclipsify_lib/generator.py
+++ src/eclipsify_lib/generator.py
@@ -17,7 +17,6 @@ import subprocess
 import io
 import StringIO
 
-
 cppAvailable = subprocess.call(["sh", "-c", "echo | cpp > /dev/null 2>&1"]) == 0
 if not cppAvailable:
     print(colored("""Warning:
@@ -26,28 +25,27 @@ if not cppAvailable:
     On Ubuntu or Debian run 'sudo apt-get install cpp' to install it.
     """, 'red'))
 
-
 def applyCppAndStripCommentsIntoFile(fileName, definitions, outfile, useCpp):
     stderr = None
     if useCpp:
         args = ''
         for key, value in definitions.iteritems():
             args += "'-D%s=%s' " % (key, value)
-        pipe = subprocess.Popen("cpp -traditional-cpp %s '%s'" %(args, fileName), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        pipe = subprocess.Popen("cpp -traditional-cpp %s '%s'" % (args, fileName), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         source = pipe.stdout
         stderr = pipe.stderr
     else:
         source = io.open(fileName, 'r')
-    
+
     for line in source.readlines():
         if line != '\n' and not commentFilter.match(line):
-            print(line, end='', file = outfile)
-            
+            print(line, end='', file=outfile)
+
     if stderr:
         for line in stderr.readlines():
             print(colored(line, 'red'), end='')
         stderr.close()
-    
+
     source.close()
 
 def applyCppAndStripComments(fileName, definitions, useCpp):
@@ -58,23 +56,25 @@ def applyCppAndStripComments(fileName, definitions, useCpp):
     return value
 
 class ProjectFile:
-    def __init__(self, name, dirPath = None):
+
+    def __init__(self, name, dirPath=None):
         self._name = name;
         if isinstance(dirPath, string_types):
             self._dir = dirPath
         else:
             self._dir = os.path.join(*dirPath) if dirPath else ''
-    
+
     def getFullDirPath(self, baseDir):
         return os.path.join(baseDir, self._dir)
-        
-    def getFullPath(self, baseDir = ""):
+
+    def getFullPath(self, baseDir=""):
         return os.path.join(self.getFullDirPath(baseDir), self._name)
 
     def __str__(self):
         return self.getFullPath();
 
 class ProjectFilesGenerator:
+
     def __init__(self, verbose, name, srcDir, buildDir, cppMacros) :
         self._verbose = verbose
         self._name = name
@@ -85,12 +85,12 @@ class ProjectFilesGenerator:
     def error(self, text):
         error(text)
         self.errors += 1
-        
-    def generate(self, searchDirs, projectFiles, outputDir, forceOverwrite = False):
+
+    def generate(self, searchDirs, projectFiles, outputDir, forceOverwrite=False):
         self.errors = 0
-        
+
         print("-- Generating project %s in directory '%s'" % (self._name, outputDir))
-        verbose("--- Using template search path '%s'" % ( ":".join(searchDirs)))
+        verbose("--- Using template search path '%s'" % (":".join(searchDirs)))
         self.createDir(outputDir, '--')
         for projectFile in projectFiles :
             projectFileFullPath = projectFile.getFullPath(outputDir)
@@ -101,7 +101,7 @@ class ProjectFilesGenerator:
                 try:
                     content = self.calcContent(searchDirs, projectFile.getFullPath());
                     with open(projectFileFullPath, 'w') as outfile:
-                        print (content , file = outfile )
+                        print(content , file=outfile)
                     okay("--> OK")
                 except tools.FindError as e:
                     self.error("--> Failed: Could not find template file '%s' in search list %s!" % (e.file, searchDirs))
@@ -124,4 +124,4 @@ class ProjectFilesGenerator:
         filePath = tools.findInSearchPath(searchDirs, fileName)
         verbose("---- Instantiating template file '%s'" % filePath);
         content = applyCppAndStripComments(filePath, self._cppMacros, cppAvailable)
-        return content.format(name = self._name, buildDir = self._buildDir, srcDir = self._srcDir)
+        return content.format(name=self._name, buildDir=self._buildDir, srcDir=self._srcDir)
