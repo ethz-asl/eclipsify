628e3985a3db534506471b355a411359c358f11c
Hannes Sommer <hannes.sommer@mavt.ethz.ch>
2019-07-11 10:03:28 +0200

    Completed list of packages in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['eclipsify_lib', 'eclipsify_lib.templates'],
+    packages=[
+        'eclipsify_lib',
+        'eclipsify_lib.tools',
+        'eclipsify_lib.package_tools',
+        'eclipsify_lib.templates'],
     package_dir={'':'src'},
     scripts=['src/eclipsify-gen-project','src/eclipsify'],
     package_data={'eclipsify_lib.templates': ['linux2/.cproject',
